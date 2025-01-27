d3e219649200a86cbcafe23c8dbf84dd4085fecd
Pavel Anikin <pavel.anikin@netcracker.com>
2025-01-27 16:30:08 +0400

    refactor: change run function to a non-async function for improved performance

--- actions/commit-and-push/index.js
+++ actions/commit-and-push/index.js
@@ -2,14 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { exec } = require('child_process');
 
-async function run() {
+function run() {
   try {
 
     const email = core.getInput('email') || 'tech@qubership.com';
     const user = core.getInput('user') || 'tech';
     const commitMessage = core.getInput('commitMessage') || 'Update pom.xml';
 
-    
     exec(`git config --global user.email "${email}"`);
     exec(`git config --global user.name "${user}"`);
     exec(`git add .`);
