56535284b5a70f9304f36c7d1f8635a79b7b0c48
Pavel Anikin <pavel.anikin@netcracker.com>
2025-01-27 14:32:43 +0400

    feat: refactor XML update logic and log updated XML content in xml-replace module

--- actions/xml-replace/index.js
+++ actions/xml-replace/index.js
@@ -1,58 +1,61 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
 const { DOMParser, XMLSerializer } = require('xmldom');
 const xpath = require('xpath');
-
 const fs = require('fs');
+
+
+
+async function updateXml(filePath, path, newValue) {
+
+  const select = xpath.useNamespaces({
+    p: 'http://maven.apache.org/POM/4.0.0'
+  });
+
+  const xml = fs.readFileSync(filePath, 'utf8');
+  core.info(`Original XML: ${xml}`);
+
+  const doc = new DOMParser().parseFromString(xml);
+
+  const nodes = select(xpathExpression, doc);
+
+  if (nodes.length === 0) {
+    throw new Error(`No nodes found for expression: ${xpathExpression}`);
+  }
+
+  nodes.forEach((node) => {
+    node.textContent = newValue;
+  });
+
+  const serialized = new XMLSerializer().serializeToString(doc);
+  fs.writeFileSync(filePath, serialized);
+
+  const updatedXml = fs.readFileSync(filePath, 'utf8');
+  core.info(`Updated XML: ${updatedXml}`);
+}
+
 
 
 async function run() {
   try {
 
-    // const name = core.getInput('name');
-    // const greeting = core.getInput('greeting');
+    // const filePath = core.getInput('filePath');
+    // const path = core.getInput('path');
+    // const newValue = core.getInput('newValue');
+    // const commit = core.getInput('commit');
+    // const email = core.getInput('email');
+    // const user = core.getInput('user');
 
 
     // core.info(`Hello, ${name}!`);
     // core.info(`My boy: ${greeting}`);
 
-    const select = xpath.useNamespaces({
-      p: 'http://maven.apache.org/POM/4.0.0'
-    });
+    const filePath = './pom.xml';
+    const path = '//p:project/p:properties/p:revision';
+    const newValue = '1.0.0';
 
-    let filePath = './pom.xml'
-
-    let path = '//p:project/p:properties/p:revision'
-    let replacement = '1.0.0'
-
-    const xml = fs.readFileSync(filePath, 'utf8');
-
-    core.info(`Original XML: ${xml}`);
-
-    const doc = new DOMParser().parseFromString(xml);
-
-    const nodes = select(path, doc);
-
-
-    if (nodes.length === 0) {
-      throw new Error(`No nodes found for expression: ${path}`);
-    }
-
-    nodes.forEach((node) => {
-      node.textContent = replacement;
-    });
-
-
-    const serialized = new XMLSerializer().serializeToString(doc);
-    fs.writeFileSync(filePath, serialized);
-
-
-    const xml2 = fs.readFileSync(filePath, 'utf8');
-    core.info(`Updated XML: ${xml2}`);
-    // const context = github.context;
-    // core.info(`Event: ${context.eventName}`);
-
+    // Вызываем нашу функцию
+    await updateXml(filePath, path, newValue);
 
     // core.setOutput('done', `${name}`);
   } catch (error) {
